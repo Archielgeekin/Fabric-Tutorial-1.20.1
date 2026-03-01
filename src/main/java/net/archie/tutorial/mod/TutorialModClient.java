a90d9ab418cd5a3609d55a893fefc637fed93772
ARCHIE <wapewastaken@Gmail.com>
2026-03-01 23:44:29 +0630

    very im

---
+++ src/main/java/net/archie/tutorial/mod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.archie.tutorial.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
